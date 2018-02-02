4083056992b391faf5e919f9318a02e2d19a7b55
Gergely Iharosi <iharosi@gmail.com>
2018-02-02 16:30:20 +0100

    Easier console info coloring

--- helpers/service.js
+++ helpers/service.js
@@ -1,46 +1,26 @@
 'use strict';
 
-const moment = require('moment');
-const colors = require('./colors');
-
-/*
- * @param {String} text A string which needs to be colorized
- * @param {String} alter Alternate the text with color or light. Available values:
-    dim, bright, black, red, green, yellow, blue, magenta, cyan, white
- */
-const formatText = (text = '', alter = 'dim') => {
-    let format = {
-        reset: colors.reset,
-        dim: colors.dim,
-        bright: colors.bright,
-        black: colors.fgBlack,
-        red: colors.fgRed,
-        green: colors.fgGreen,
-        yellow: colors.fgYellow,
-        blue: colors.fgBlue,
-        magenta: colors.fgMagenta,
-        cyan: colors.fgCyan,
-        white: colors.fgWhite
-    };
-
-    return (format[alter] || format.dim) + text + format.reset;
-};
+const timestamp = require('time-stamp');
+const c = require('cli-color');
 
 /*
  * @param {String} message Console message
  * @param {String} messageType Can be the following: 'info', 'warning', 'error', 'highlight'
  */
 const log = (message, messageType = 'info') => {
-    let formattedDate = moment().format('YYYY-MM-DD HH:mm:ss');
+    let formattedDate = timestamp('YYYY-MM-DD HH:mm:ss');
     let types = {
-        info: 'dim',
+        info: 'white',
         warning: 'yellow',
         error: 'red',
-        highlight: 'green'
+        highlight: 'cyan'
     };
 
     if (message) {
-        console.log(`${formattedDate} ${formatText(message, types[messageType])}`);
+        console.log([
+            c.blackBright(formattedDate),
+            c[types[messageType]](message)
+        ].join(' '));
     }
 };
 
