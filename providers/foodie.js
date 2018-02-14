c511d130b8c3348678453d6914273f3017354744
Gergely Iharosi <iharosi@gmail.com>
2018-02-14 01:13:06 +0100

    Finalize foodie provider
    closes #3

--- providers/foodie.js
+++ providers/foodie.js
@@ -1,70 +1,117 @@
 'use strict';
 
-const request = require('request');
+const _ = require('lodash');
+const url = require('url');
 const moment = require('moment');
-const options = {
-    url: 'https://graph.facebook.com/v2.11/494549960697458/posts?limit=5&access_token=' +
-        process.env.FB_ACCESS_TOKEN,
+const request = require('request-promise-native');
+const fbGrapApiUrl = 'https://graph.facebook.com/v2.12';
+const fbId = '494549960697458';
+const defaultOptions = {
+    qs: {
+        access_token: process.env.FB_ACCESS_TOKEN // eslint-disable-line camelcase
+    },
     headers: {
         'User-Agent': [
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)',
             'AppleWebKit/604.5.6 (KHTML, like Gecko)',
             'Version/11.0.3 Safari/604.5.6'
         ].join(' ')
+    },
+    json: true
+};
+
+const getDataFromPost = (fbPosts) => {
+    let todayPosts;
+    let payload = {
+        postUrl: null,
+        menu: []
+    };
+
+    if (fbPosts && fbPosts.data) {
+        todayPosts = fbPosts.data
+            .filter((post) => {
+                return post.message.indexOf('Leveseink') >= 0;
+            })
+            .filter((post) => {
+                return moment().isSame(moment(post.created_time), 'day');
+            });
     }
+
+    if (todayPosts.length) {
+        payload.postUrl = todayPosts[0].permalink_url;
+        payload.menu = payload.menu.concat(todayPosts[0].message
+            .slice(todayPosts[0].message.indexOf('Leveseink'))
+            .split('\n')
+            .filter((line, index, array) => {
+                let keep = true;
+                let previous = array[index - 1];
+
+                if (line.charAt() !== '-' && previous && previous.charAt() === '-') {
+                    keep = false;
+                }
+
+                return keep;
+            }));
+    }
+
+    return payload;
+};
+
+const getDataFromPage = (page) => {
+    return {
+        name: _.get(page, 'name', null)
+    };
+};
+
+const getDataFromPicture = (picture) => {
+    return {
+        logo: _.get(picture, 'data.url', null)
+    };
 };
 
 const fetch = () => {
     return new Promise((resolve, reject) => {
-        request(options, (error, response, body) => {
-            if (error || response.statusCode !== 200) {
-                reject(error);
-            } else {
-                let fbPosts;
-                let post;
-                let menu;
-
-                try {
-                    fbPosts = JSON.parse(body);
-                } catch (error) {
-                    reject(error);
-                }
-
-                post = fbPosts.data
-                    .filter((post) => {
-                        return post.message.indexOf('Leveseink') >= 0;
-                    })
-                    .filter((post) => {
-                        let now = moment().format('YYYY-MM-DD');
-                        let createdTime = post.created_time.split('T')[0];
-
-                        return now === createdTime;
-                    })[0];
-
-                menu = post ? post.message
-                    .slice(post.message.indexOf('Leveseink'))
-                    .split('\n')
-                    .filter((line, index, array) => {
-                        let keep = true;
-                        let previous = array[index - 1];
-
-                        if (line.charAt() !== '-' && previous && previous.charAt() === '-') {
-                            keep = false;
-                        }
-
-                        return keep;
-                    }) : '';
-
-                resolve({
-                    id: 'foodie',
-                    name: 'Foodie MinuteBistro',
-                    logo: '',
-                    url: post ? 'https://facebook.com/' + post.id : '',
-                    menu: menu,
-                    timestamp: moment().format('YYYY-MM-DD HH:mm:ss')
-                });
+        let optionForPosts = {
+            uri: url.resolve(fbGrapApiUrl, 'posts'),
+            qs: {
+                id: fbId,
+                fields: 'created_time,message,permalink_url',
+                limit: 5
             }
-        });
+        };
+        let optionForPage = {
+            uri: url.resolve(fbGrapApiUrl, fbId)
+        };
+        let optionForPicture = {
+            uri: url.resolve(fbGrapApiUrl, `${fbId}/picture`),
+            qs: {
+                redirect: false,
+                type: 'large'
+            }
+        };
+
+        Promise.all([
+            request(_.merge({}, defaultOptions, optionForPosts)),
+            request(_.merge({}, defaultOptions, optionForPage)),
+            request(_.merge({}, defaultOptions, optionForPicture))
+        ]).then((responses) => {
+            return _.merge(
+                {},
+                getDataFromPost(responses[0]),
+                getDataFromPage(responses[1]),
+                getDataFromPicture(responses[2])
+            );
+        }).then((payload) => {
+            resolve({
+                id: 'foodie',
+                name: payload.name,
+                logo: payload.logo,
+                url: payload.postUrl,
+                menu: payload.menu,
+                timestamp: moment().format()
+            });
+        })
+        .catch(reject);
     });
 };
 
