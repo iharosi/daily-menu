144601a7e60695bbabc11d6012b8f56037fa809a
Gergely Iharosi <iharosi@gmail.com>
2018-02-15 21:32:07 +0100

    Update BK logo

--- providers/burgerking.js
+++ providers/burgerking.js
@@ -17,8 +17,10 @@ const fetch = () => {
         resolve({
             id: 'burgerking',
             name: 'BURGER KING® Hungary',
-            logo: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Burger_King_Logo.svg',
-            url: 'http://burgerking.hu/sites/burgerking.hu/files/HetkozNapiBKmenu_Mindentermek_lista_1000x550px.jpg',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/' +
+                'Burger_King_Logo.svg/500px-Burger_King_Logo.svg.png',
+            url: 'http://burgerking.hu/sites/burgerking.hu/files/' +
+                'HetkozNapiBKmenu_Mindentermek_lista_1000x550px.jpg',
             menu: ['Hétköznapi Burger King menü:',
                 subOfTheDay[moment().isoWeekday()]],
             lastUpdated: moment().format()
