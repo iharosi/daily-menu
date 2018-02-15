48b8e916d3adbcbd3a37df1f183990c5615c0d5c
Gergely Iharosi <iharosi@gmail.com>
2018-02-15 21:35:13 +0100

    Update Subway logo

--- providers/subway.js
+++ providers/subway.js
@@ -17,7 +17,8 @@ const fetch = () => {
         resolve({
             id: 'subway',
             name: 'Subway',
-            logo: 'https://www.subwayhungary.com/images/topnav/SUBWAY_Y_G.svg',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/' +
+                'Subway_2016_logo.svg/640px-Subway_2016_logo.svg.png',
             url: 'https://www.subwayhungary.com/images/menu/WEB_NYITO_SOTD_644x360.jpg',
             menu: ['Sub of the Day:',
                 subOfTheDay[moment().isoWeekday()]],
